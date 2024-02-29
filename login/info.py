1e0e84ffe839eb08fb8b4487567285bd86b5d145
ShriDhathri <dhathridhatu@gmail.com>
2024-02-29 19:33:43 +0530

    loginPage

---
+++ login/info.py
@@ -0,0 +1,6 @@
+# Contains information about email
+EMAIL_USE_TLS=True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER='dhathri.work@gmail.com'
+EMAIL_HOST_PASSWORD='paik olih pdme ools'
+EMAIL_PORT=587
